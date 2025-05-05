cac079ab224fcdf65fc8297924e0985de7b3a874
Siyuan Wu <wusiyuan18@mails.ucas.ac.cn>
2025-05-05 14:26:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(api_key:str,
     day = datetime.datetime.now()
     url = "https://rss.arxiv.org/rss/cond-mat"
     feed = feedparser.parse(url)
-    update_date = datetime.datetime.strptime("-".join(feed['feed']['published'].split(' ')[1:4]),'%d-%b-%Y')
+    update_date = datetime.datetime.fromtimestamp(time.mktime(entry['published_parsed']))
     if update_date.strftime("%Y-%m-%d") == day.strftime("%Y-%m-%d"):
         content = ""
         for i,entry in enumerate(feed.entries):
